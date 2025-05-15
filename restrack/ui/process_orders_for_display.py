d8c181edfd63f1dcf7d3bf8eceba873a9b5b1eb3
vvcb <vvcb.n1@gmail.com>
2025-05-15 01:41:03 +0100

    Add order status colors - needs improvement

--- restrack/ui/process_orders_for_display.py
+++ restrack/ui/process_orders_for_display.py
@@ -1,57 +1,83 @@
 import pandas as pd
 import panel as pn
+from bokeh.models.widgets import HTMLTemplateFormatter
 
-def process_orders_for_display(df):       
-    df_to_view = df[["order_id",
-                    "patient_id",
-                    "proc_name",
-                    "current_status",
-                    "order_datetime",
-                    "in_progress",
-                    "partial",
-                    "complete",
-                    "supplemental",
-                    "status",
-                    "user_note"
-                    ]]
-    
-    
-                    
-    df_to_view['order_datetime'] = pd.to_datetime(df_to_view['order_datetime'], format='mixed')
-    df_to_view['order_datetime'] = df_to_view['order_datetime'].dt.strftime('%d/%m/%Y')
-    df_to_view["in_progress"] = pd.to_datetime(df_to_view[ "in_progress"], format='mixed')
-    df_to_view["in_progress"] = df_to_view[ "in_progress"].dt.strftime('%d/%m/%Y')
-    df_to_view["partial"] = pd.to_datetime(df_to_view[ "partial"], format='mixed')
-    df_to_view["partial"] = df_to_view[ "partial"].dt.strftime('%d/%m/%Y')
-    df_to_view["complete"] = pd.to_datetime(df_to_view[ "complete"], format='mixed')
-    df_to_view["complete"] = df_to_view["complete"].dt.strftime('%d/%m/%Y')
-    df_to_view["supplemental"] = pd.to_datetime(df_to_view["supplemental"], format='mixed')
-    df_to_view["supplemental"] = df_to_view["supplemental"].dt.strftime('%d/%m/%Y')
 
-       # Replace NaN values with "-"
+def process_orders_for_display(df):
+    df_to_view = df[
+        [
+            "order_id",
+            "patient_id",
+            "proc_name",
+            "current_status",
+            "order_datetime",
+            "in_progress",
+            "partial",
+            "complete",
+            "supplemental",
+            "status",
+            "user_note",
+        ]
+    ]
+
+    df_to_view["order_datetime"] = pd.to_datetime(
+        df_to_view["order_datetime"], format="mixed"
+    )
+    df_to_view["order_datetime"] = df_to_view["order_datetime"].dt.strftime("%d/%m/%Y")
+    df_to_view["in_progress"] = pd.to_datetime(
+        df_to_view["in_progress"], format="mixed"
+    )
+    df_to_view["in_progress"] = df_to_view["in_progress"].dt.strftime("%d/%m/%Y")
+    df_to_view["partial"] = pd.to_datetime(df_to_view["partial"], format="mixed")
+    df_to_view["partial"] = df_to_view["partial"].dt.strftime("%d/%m/%Y")
+    df_to_view["complete"] = pd.to_datetime(df_to_view["complete"], format="mixed")
+    df_to_view["complete"] = df_to_view["complete"].dt.strftime("%d/%m/%Y")
+    df_to_view["supplemental"] = pd.to_datetime(
+        df_to_view["supplemental"], format="mixed"
+    )
+    df_to_view["supplemental"] = df_to_view["supplemental"].dt.strftime("%d/%m/%Y")
+
+    # Replace NaN values with "-"
     df_to_view = df_to_view.fillna("-")
-    
-    df_to_view.rename(columns={
-                        "proc_name":"Investigation",
-                        "current_status":"Status",
-                        "order_datetime":"Ordered",
-                        "in_progress": "In progress",
-                        "status":"User Status",
-                        "user_note": "User Note"
-                        
-                    },inplace=True)
-    
-    tbl = pn.widgets.Tabulator(
-            df_to_view,
-            layout='fit_data_stretch',
-            groupby=["patient_id"],
-            hidden_columns=["index","order_id", "patient_id"],
-            pagination="local",
-            page_size=6,
-            selectable="checkbox",
-            disabled=True
+
+    df_to_view.rename(
+        columns={
+            "proc_name": "Investigation",
+            "current_status": "Status",
+            "order_datetime": "Ordered",
+            "in_progress": "In progress",
+            "status": "User Status",
+            "user_note": "User Note",
+        },
+        inplace=True,
     )
 
+    status_format_template = """
+    <b><div style="background:
+        <%= (function colorfromint(){
+            if( Status == 6){
+                return("Green")
+            }
+            else{
+                return("Red")
+            }
+         }()) %>;"> <%= Status %> </div></b>
+    """
+    # status_format_template = """
+    #     <b><%= Status %></b>"""
+    status_formatter = HTMLTemplateFormatter(template=status_format_template)
+
+    tbl = pn.widgets.Tabulator(
+        df_to_view,
+        layout="fit_data_stretch",
+        groupby=["patient_id"],
+        hidden_columns=["index", "order_id", "patient_id"],
+        pagination="local",
+        page_size=6,
+        selectable="checkbox",
+        disabled=True,
+        formatters={"Status": status_formatter},
+    )
 
     return tbl
     # def colour_rows(row):
@@ -59,12 +85,7 @@ def process_orders_for_display(df):
     #         return ['background-color: green'] * len(row)
     #     else:
     #         return [''] * len(row)
-    
+
     # styled = tbl.style.map(colour_rows, axis=1)
 
     # return styled
-  
-        
-    
-    
-   
